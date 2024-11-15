439cab8a0c3daac533337702dfacdc8d74bcdca9
Franco Algañaraz <142274105+francoarga@users.noreply.github.com>
2024-11-15 16:25:48 -0300

    Update ComparadorRedituabilidad.java

--- ComparadorRedituabilidad.java
+++ ComparadorRedituabilidad.java
@@ -1,9 +1,19 @@
 package aed;
 
+import java.util.Comparator;
 
-public class ComparadorRedituabilidad<Traslado> implements Comparable<Traslado>{
-public int compare(Traslado t1, Traslado t2){
-return Integer.compare(t1.gananciaNeta(), t2.gananciaNeta());
+public class ComparadorRedituabilidad implements Comparator<Traslado> {
+    @Override
+    public int compare(Traslado t1, Traslado t2) {
+        return Integer.compare(t2.gananciaNeta, t1.gananciaNeta);
+    }
 }
+
+public class ComparadorAntiguedad implements Comparator<Traslado> {
+    @Override
+    public int compare(Traslado t1, Traslado t2) {
+        // Orden ascendente por timestamp
+        return Integer.compare(t1.timestamp, t2.timestamp);
+    }
 }
 
